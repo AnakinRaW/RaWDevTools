00f666d8d4ace0daa6ca8b3b9aacbedc01ddbaa4
AnakinRaW <anakin@gmx-topmail.de>
2023-05-11 19:06:10 +0200

    empty tool code added

---
+++ src/DevLauncher/DevLauncherEnvironment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using AnakinRaW.ApplicationBase;
+
+namespace RepublicAtWar.DevLauncher;
+
+internal class DevLauncherEnvironment : ApplicationEnvironmentBase
+{
+    private const string ToolPathName = "RawDevLauncher";
+
+    public DevLauncherEnvironment(Assembly assembly, IServiceProvider serviceProvider) : base(assembly, serviceProvider)
+    {
+    }
+
+    public override string ApplicationName => "Republic at War DevLauncher";
+    public override Uri? RepositoryUrl => null;
+    public override ICollection<Uri> UpdateMirrors { get; } = new List<Uri>
+    {
+        new($"https://republicatwar.com/downloads/{ToolPathName}")
+    };
+    public override string ApplicationRegistryPath => $@"SOFTWARE\{ToolPathName}";
+    protected override string ApplicationLocalDirectoryName => ToolPathName;
+}
--- src/DevLauncher/Program.cs
+++ src/DevLauncher/Program.cs
@@ -1,9 +1,37 @@
 ﻿using System;
+using System.Reflection;
+using AnakinRaW.ApplicationBase;
+using AnakinRaW.CommonUtilities.Registry;
+using AnakinRaW.CommonUtilities.Registry.Windows;
+using Microsoft.Extensions.DependencyInjection;
 
-internal class Program
+namespace RepublicAtWar.DevLauncher;
+
+internal class Program : CliBootstrapper
 {
-    public static void Main(string[] args)
+    public static int Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        return new Program().Run(args);
+    }
+
+    protected override IApplicationEnvironment CreateEnvironment(IServiceProvider serviceProvider)
+    {
+        return new DevLauncherEnvironment(Assembly.GetExecutingAssembly(), serviceProvider);
+    }
+
+    protected override IRegistry CreateRegistry()
+    {
+        return new WindowsRegistry();
+    }
+
+    protected override int ExecuteAfterUpdate(string[] args, IServiceCollection serviceCollection)
+    {
+        var sp = serviceCollection.BuildServiceProvider();
+        var env = sp.GetRequiredService<IApplicationEnvironment>();
+
+        Console.WriteLine($"Current Version: {env.AssemblyInfo.FileVersion}");
+        Console.WriteLine("Custom Tool Code would have been executed. Press enter to exit.");
+        Console.ReadLine();
+        return 0;
     }
 }
