8e7da79647ca2a075bc5ce82ceea5814224b8414
AnakinRaW <anakin@gmx-topmail.de>
2023-05-16 14:51:16 +0200

    start the mod

--- src/DevLauncher/Program.cs
+++ src/DevLauncher/Program.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.IO.Abstractions;
 using System.Reflection;
 using AnakinRaW.ApplicationBase;
 using AnakinRaW.CommonUtilities.Registry;
 using AnakinRaW.CommonUtilities.Registry.Windows;
+using EawModinfo.Model;
+using EawModinfo.Spec;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using PetroGlyph.Games.EawFoc.Clients.Steam;
+using PetroGlyph.Games.EawFoc.Clients;
+using PetroGlyph.Games.EawFoc;
+using PetroGlyph.Games.EawFoc.Games;
+using PetroGlyph.Games.EawFoc.Services;
+using PetroGlyph.Games.EawFoc.Services.Dependencies;
+using PetroGlyph.Games.EawFoc.Services.Detection;
+using PetroGlyph.Games.EawFoc.Services.Name;
 
 namespace RepublicAtWar.DevLauncher;
 
@@ -28,12 +40,60 @@ internal class Program : CliBootstrapper
 
     protected override int ExecuteAfterUpdate(string[] args, IServiceCollection serviceCollection)
     {
-        var sp = serviceCollection.BuildServiceProvider();
-        var env = sp.GetRequiredService<IApplicationEnvironment>();
+        var services = CreateServices(serviceCollection);
 
-        Console.WriteLine($"Current Version: {env.AssemblyInfo.FileVersion}");
-        Console.WriteLine("Custom Tool Code would have been executed. Press enter to exit.");
-        Console.ReadLine();
+        var fileSystem = services.GetRequiredService<IFileSystem>();
+        var logger = services.GetService<ILoggerFactory>()?.CreateLogger(GetType());
+
+        var currentDirectory = fileSystem.DirectoryInfo.New(Environment.CurrentDirectory);
+       
+        // Assuming the currentDir is inside a Mod's directory, we need to go up two level (Game/Mods/ModDir)
+        var potentialGameDirectory = currentDirectory.Parent?.Parent;
+        if (potentialGameDirectory is null)
+            throw new GameException("Unable to find game installation: Wrong install path?");
+
+        var gd = new DirectoryGameDetector(potentialGameDirectory, services);
+        var detectedGame = gd.Detect(new GameDetectorOptions(GameType.Foc));
+
+        if (detectedGame.GameLocation is null)
+            throw new GameException("Unable to find game installation: Wrong install path?");
+
+        if (detectedGame.Error is not null)
+            throw new GameException($"Unable to find game installation: {detectedGame.Error.Message}", detectedGame.Error);
+
+        logger?.LogInformation($"Found game {detectedGame.GameIdentity} at '{detectedGame.GameLocation}'");
+
+        var gameFactory = new GameFactory(services);
+        var game = gameFactory.CreateGame(detectedGame);
+        
+        var rawDevRef = new ModReference(currentDirectory.FullName, ModType.Default);
+
+        var mf = new ModFactory(services).FromReference(game, rawDevRef, true);
+        
+        game.AddMod(mf);
+
+        var client = services.GetRequiredService<IGameClientFactory>().CreateClient(game.Platform, services);
+
+        client.Play(mf);
+
         return 0;
+    }
+
+    private static IServiceProvider CreateServices(IServiceCollection serviceCollection)
+    {
+        PetroglyphGameInfrastructureLibrary.InitializeLibraryWithDefaultServices(serviceCollection);
+        PetroglyphClientsLibrary.InitializeLibraryWithDefaultServices(serviceCollection);
+        PetroglyphWindowsSteamClientsLibrary.InitializeLibraryWithDefaultServices(serviceCollection);
+
+        serviceCollection.AddTransient<IGameDetector>(sp => new SteamPetroglyphStarWarsGameDetector(sp));
+        serviceCollection.AddTransient<IGameFactory>(sp => new GameFactory(sp));
+        serviceCollection.AddTransient<IModReferenceFinder>(sp => new FileSystemModFinder(sp));
+        serviceCollection.AddTransient<IModFactory>(sp => new ModFactory(sp));
+        serviceCollection.AddTransient<IModReferenceLocationResolver>(sp => new ModReferenceLocationResolver(sp));
+        serviceCollection.AddTransient<IModNameResolver>(sp => new DirectoryModNameResolver(sp));
+        serviceCollection.AddTransient<IDependencyResolver>(sp => new ModDependencyResolver(sp));
+        serviceCollection.AddTransient<IGameClientFactory>(sp => new DefaultGameClientFactory(sp));
+
+        return serviceCollection.BuildServiceProvider();
     }
 }
