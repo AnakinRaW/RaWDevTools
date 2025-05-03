f9a821e666bc7a42978021f9d2bca3648ad6ede9
AnakinRaW <anakin@gmx-topmail.de>
2025-05-03 17:43:35 +0200

    start cli app

--- src/DevLauncher/NewProgram.cs
+++ src/DevLauncher/NewProgram.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.IO.Abstractions;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using AnakinRaW.ApplicationBase;
 using AnakinRaW.ApplicationBase.Environment;
 using AnakinRaW.ApplicationBase.Update;
 using AnakinRaW.AppUpdaterFramework;
+using AnakinRaW.AppUpdaterFramework.External;
+using AnakinRaW.AppUpdaterFramework.Handlers;
 using AnakinRaW.AppUpdaterFramework.Metadata.Product;
 using AnakinRaW.AppUpdaterFramework.Product;
-using AnakinRaW.AppUpdaterFramework.Updater;
 using AnakinRaW.CommonUtilities;
 using AnakinRaW.CommonUtilities.Registry;
 using AnakinRaW.CommonUtilities.Registry.Windows;
+using AnakinRaW.ExternalUpdater.Services;
+using CommandLine;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Testably.Abstractions;
 
 namespace RepublicAtWar.DevLauncher;
@@ -50,15 +53,38 @@ internal class NewProgram : SelfUpdateableAppLifecycle
         Console.WriteLine(string.Join(" ", args));
         Console.WriteLine($"Elevated: {CurrentProcessInfo.Current.IsElevated}");
 
+        var p = new Parser(with =>
+        {
+            with.IgnoreUnknownArguments = true;
+        });
+
+        var parseResult = p.ParseArguments<A>(args);
+
+        parseResult.WithParsed(o =>
+        {
+            Console.WriteLine(o.Value);
+        });
+
+        var other  = p.ParseArguments<B, C>(args);
+        other.WithParsed<B>(b => Console.WriteLine("B: " + b.Value));
+        other.WithParsed<C>(c => Console.WriteLine("C: " + c.Value));
+        other.WithNotParsed(errors => Console.WriteLine("Errors: " + string.Join(", ", errors.Select(e => e.ToString()))));
+
 #if NETFRAMEWORK
 
         var ps = appServiceProvider.GetRequiredService<IProductService>();
-        var us = appServiceProvider.GetRequiredService<IUpdateService>();
+        var uh = appServiceProvider.GetRequiredService<IUpdateHandler>();
+        var bm = appServiceProvider.GetRequiredService<IBranchManager>();
+        var eus = appServiceProvider.GetRequiredService<IExternalUpdaterService>();
+        var eul = appServiceProvider.GetRequiredService<IExternalUpdaterLauncher>();
 
-        //var ip = ps.GetCurrentInstance();
-        //var e = await us.CheckForUpdatesAsync();
+        var bl = await bm.GetAvailableBranchesAsync();
+        var ip = ps.GetCurrentInstance();
+        var e = await uh.CheckForUpdateAsync(new ProductReference(ip.Name, ip.Version, bl.FirstOrDefault(x => x.IsDefault)));
 
-        Console.WriteLine();
+
+        var ro = eus.CreateRestartOptions(true);
+        eul.Start(eus.GetExternalUpdater(), ro);
 #endif
 
 
@@ -76,6 +102,31 @@ internal class NewProgram : SelfUpdateableAppLifecycle
         services.MakeAppUpdateable(
             UpdatableApplicationEnvironment!, 
             sp => new CosturaApplicationProductService(ApplicationEnvironment, sp),
-            sp => new JsonManifestLoader(sp));
+            sp => new JsonManifestLoader(sp), sc =>
+            {
+
+            });
     }
+}
+
+[Verb("update")]
+public class A
+{
+    [Option('o')]
+    public string Value { get; set; }
+}
+
+
+[Verb("verify")]
+public class B
+{
+    [Option('o')]
+    public string Value { get; set; }
+}
+
+[Verb("other")]
+public class C
+{
+    [Option('o')]
+    public string Value { get; set; }
 }
